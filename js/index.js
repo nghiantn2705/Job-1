a8a9c10e6d716f626ac6b6bb82ba13e1709aa2d5
nghiantn2705 <117350106+nghiantn2705@users.noreply.github.com>
2023-04-28 00:48:21 +0700

    nghia

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+var countDownDate = new Date("April 30, 2023 23:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("box-time-d").innerHTML = days   
+  document.getElementById("box-time-h").innerHTML = hours 
+  document.getElementById("box-time-m").innerHTML = minutes 
+  document.getElementById("box-time-s").innerHTML = seconds 
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
